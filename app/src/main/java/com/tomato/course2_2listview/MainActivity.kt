31219afbd41d8f0a2b1566fb94cbd9af70671ccb
zokeez <birukov.alex@gmail.com>
2022-07-07 13:19:23 +0500

    Initial commit

---
+++ app/src/main/java/com/tomato/course2_2listview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tomato.course2_2listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
